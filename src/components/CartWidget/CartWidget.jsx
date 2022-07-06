ba68663669d651666b7f86ebbb320bd876c9e8fb
Alan Burga <ahbb21@hotmail.com>
2022-07-05 22:24:25 -0500

    Re-entrega Proyecto Final

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,16 +3,12 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 import { GlobalContext } from "../../context/CartContext";
 
 export default function CartIcon() {
-  const { carrito, showCart } = useContext(GlobalContext);
+  const { carrito } = useContext(GlobalContext);
 
   return (
     <div className="nav__menu--cart">
-      {/* {showCart > 0 ? ( */}
-      <>
-        <MdOutlineShoppingCart size="1.5em" />
-        <span className="cart-q">{carrito.reduce((a, b) => a + b.q, 0)}</span>
-      </>
-      {/* ) : null} */}
+      <MdOutlineShoppingCart size="1.5em" />
+      <span className="cart-q">{carrito.reduce((a, b) => a + b.q, 0)}</span>
     </div>
   );
 }
